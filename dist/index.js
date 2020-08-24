5cac10f7883cc609f1e0b5b38f9cc4d5e29691b4
Aaron <aaron@primevox.net>
2020-08-24 15:36:04 -0500

    forgot dotenv

--- dist/index.js
+++ dist/index.js
@@ -13,6 +13,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require('dotenv').config();
 const cli_1 = __importDefault(require("./cli"));
 __exportStar(require("./api"), exports);
 __exportStar(require("./types"), exports);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import cli from './cli';
 export * from './api';
 export * from './types';
